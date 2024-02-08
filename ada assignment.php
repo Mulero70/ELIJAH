e42e1da5f7c6485601b0a518416df420b3575793
Mulero Adetutu Elijah <69467937+Mulero70@users.noreply.github.com>
2024-02-08 04:16:12 -0800

    Add files via upload

---
+++ ada assignment.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $asa = 7;
+        $i = 0;
+        $da = 4.9;
+
+        
+        $d = 0.0;
+        $str = "Mulero Adetutu";
+
+        echo $asa + $i . "<br>";
+
+        echo $da + $d ."<br>";
+
+        echo "Your name is ". $str;
+
+
+
+
+
+    ?>
+</body>
+</html>
